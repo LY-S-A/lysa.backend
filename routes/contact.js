917326c9f269417e50f7dcbd8dea25890d6fd141
lysa.dev <Lysauniverse30@gmail.com>
2025-05-03 10:27:09 +0100

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const router = express.Router();
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
